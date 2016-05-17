c954e4095219eb571ec6a707975fadf3434f21e8
George Priniski <priniskigeorge@gmail.com>
2016-05-17 16:32:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wordster::Application.config.secret_key_base = '3e7f123efc83ed08ede4aa2d141fe55b57470f98a743a6c15fd3861908cd01d991b58e46c0d79bf8dd63946fab5f0c71f49b174e12a216ad3978937559d988de'
